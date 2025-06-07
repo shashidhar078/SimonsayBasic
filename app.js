329866f8d5bb7bc575d7aa41940f73eaa07eb0a6
shashidhar078 <nagunurishashidhar@gmail.com>
2025-06-07 14:49:52 +0530

    changes done

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ document.addEventListener("keypress",function(event)
 {
     if(gameState==false)
     {
-        console.log("Game started");
+        console.log("Game has started");
+        console.log("you can play now");
         gameState=true;
     }
 })
