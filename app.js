9223a28934e7da79d7d1b9de56bfb1245c30f55f
shashidhar078 <nagunurishashidhar@gmail.com>
2025-06-07 17:20:39 +0530

    updated eventlisteners and flashy button function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let userSeq=[];
 let gameSeq=[];
-
+let color=["yellow","red","purple","green"];
+let heading=document.querySelector("h2");
 gameState=false;
 level=0;
 
@@ -11,5 +12,52 @@ document.addEventListener("keypress",function(event)
         console.log("Game has started");
         console.log("you can play now");
         gameState=true;
+        levelUp();
     }
+})
+
+function flashy(btn)
+{
+   btn.classList.add("white");
+    setTimeout(function() {
+        btn.classList.remove("white");
+    }, 250);
+}
+
+
+function levelUp()
+{
+    level++;
+    heading.innerText=`Level ${level}`;
+    let rndIndx=Math.floor(Math.random()*3);
+    let rndColor=color[rndIndx];
+    let btn=document.querySelector(`.${rndColor}`);
+    console.log(rndIndx);
+    console.log(rndColor);
+    console.log(btn);
+    flashy(btn);
+}
+
+let but1=document.querySelector("#but1");
+let but2=document.querySelector("#but2");
+let but3=document.querySelector("#but3");
+let but4=document.querySelector("#but4");
+
+
+
+but1.addEventListener("click",function(){
+    console.log("Button1 was pressed!!")
+    flashy(but1);
+})
+but2.addEventListener("click",function(){
+    console.log("Button2 was pressed!!")
+    flashy(but2);
+})
+but3.addEventListener("click",function(){
+    console.log("Button3 was pressed!!")
+    flashy(but3);
+})
+but4.addEventListener("click",function(){
+    console.log("Button4 was pressed!!")
+    flashy(but4);
 })
